55d63f8d1dd88f8ff9688e52fbf39735d9750b76
Promise Ugochukwu Nnanemere <promisennanemere@gmail.com>
2025-05-27 14:52:49 +0100

    Resolved merge conflict

---
+++ frontend/src/configs/aws-exports.ts
@@ -0,0 +1,7 @@
+export const amplifyConfig = {
+  aws_project_region: 'us-east-1',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_3vqdJciQW',
+  aws_user_pools_web_client_id: '5jiin4gdgia2habis99a90mifg',
+  };
+
